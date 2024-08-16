4cf0eb0cb192da155ab125a15b46beb774a16e07
Haywhy217 <olanipekunayomide217@gmail.com>
2024-08-16 15:41:14 +0100

    Fix Vercel deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist' 
-  },
-  server: {
-    historyApiFallback: true 
+    outDir: 'dist'
   }
 })
+
